8b04b4b1efec107e2df232c94b2323a0e1ab6016
mooyeon-choi <dus1208@ajou.ac.kr>
2021-01-07 00:24:10 +0900

    Update navbar, favicon

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ class Navbar extends Component {
         <div className="logo-nav">
           <img className="team-logo" src="https://github.com/mooyeon-choi/webzine/blob/main/public/images/team-logo.png?raw=true" alt="team-logo"/>
         </div>
-        <img src="https://github.com/mooyeon-choi/webzine/blob/main/public/images/teamlogo.png?raw=true"  alt="logo" className="navbar-logo"/>
+        <img src="https://github.com/mooyeon-choi/webzine/blob/main/public/images/title-logo.png?raw=true"  alt="logo" className="navbar-logo"/>
         <div className="menus">
           <div className="menu-icon">
             <i className="fas fa-bars"></i>

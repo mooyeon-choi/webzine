e225de8d58c5b98ca3ee16bb0bd5ab285f311779
mooyeon-choi <dus1208@ajou.ac.kr>
2021-01-07 00:02:00 +0900

    Add title logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,6 +5,7 @@ class Navbar extends Component {
     return (
       <nav className="navbar">
         <div className="logo-nav">
+          <img className="team-logo" src="https://github.com/mooyeon-choi/webzine/blob/main/public/images/team-logo.png?raw=true" alt="team-logo"/>
         </div>
         <img src="https://github.com/mooyeon-choi/webzine/blob/main/public/images/teamlogo.png?raw=true"  alt="logo" className="navbar-logo"/>
         <div className="menus">

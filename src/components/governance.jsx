56a3b38f16e0ff4377628a2a0e3d4112c8c22034
mooyeon-choi <dus1208@ajou.ac.kr>
2021-01-07 10:13:03 +0900

    Update social, governance article 내용 추가

--- src/components/governance.jsx
+++ src/components/governance.jsx
@@ -15,18 +15,14 @@ export default class Governance extends Component {
         <header className="modal-header">
         <h1 className="modal-header-title">SK의 ESG 실천노력</h1>
         <h1 className="modal-header-title">ESG와 관련해 SK 그룹이 해 온/해 나갈 것들</h1>
-          <h2 className="modal-header-subtitle">Environment</h2>
+          <h2 className="modal-header-subtitle">Governance</h2>
           <img className="center-img" src="https://github.com/mooyeon-choi/webzine/blob/main/src/images/skms.jpg?raw=true" alt="re100"/>
-          <h2 className="modal-text-title">SK는 한국 최초로 RE100에 가입하였습니다. </h2>
-          <p className="modal-text">RE100이란, Renewable Energy, 즉 재생에너지를 2050년까지 소비전력의 100퍼센트의 비율로 사용하겠다고 선언한 글로벌 캠페인입니다. RE100은 수출 경쟁력과도 직결되어 sk의 글로벌 경쟁력을 확보 할 수 있는 중요한 요소중 하나입니다.</p>
-          <img className="center-img" src="https://img.asiatoday.co.kr/file/2020y/10m/23d/2020102201001976600130961.jpg" alt="h"/>
-          <h2 className="modal-text-title">수소관련- 주식회사/건설 </h2>
-          <p className="modal-text">sk주식회사는 친환경 에너지로 성장하고있는 수소사업에 본격적으로 진출하겠다고 선언했습니다. 에너지 관련회사인 sk innovation, sk e&s 등 관계사 인력으로 구성된 수소사업추진단을 신설하여 사업 추진전략을 실행할 예정입니다.</p>
-          <img className="center-img" src="https://insight-prd-data.s3.ap-northeast-2.amazonaws.com/wp-content/uploads/2/cfile27.uf.0215234D51353F0C0D4709.jpg" alt="cloud bems"/>
-          <h2 className="modal-text-title">ICT관련 -skt</h2>
-          <p className="modal-text">sk텔레콤은 지난 2011년부터 에너지 사용을 자동으로 최적화하는 ‘클라우드 BEMS’를 개발, 사용하고 있습니다. 이는 건물 내 조명 냉·난방기 등의 센서 및 네트워크를 기반으로 중앙관리센터에 연결, 빌딩의 전력·에너지 사용량을 실시간으로 분석하는 시스템입니다.</p>
-          <h2 className="modal-text-title">환경, 사회 성과 및 목표</h2>
-          <img className="center-img" src="https://github.com/mooyeon-choi/webzine/blob/main/src/images/env-result.png?raw=true" alt="env-result"/>
+          <p className="modal-text">지배구조는 투명하고 공정한 경영을 통해 지속가능경영을 실천합니다. 기업의 목적, 이사회 역할 및 구성, 지배구조 공시 등의 요소로 나눠볼 수 있습니다</p>
+          <h2 className="modal-text-title">SK 그룹의 목적함수: 행복</h2>
+          <p className="modal-text"> SK는 경영철학인 SKMS의 개정을 통해 구성원과 이해관계자의 행복을 목적함수로 설정했습니다. 이를 통해 단순히 기업의 경제적 가치만을 좇는 것이 아니라, 구성원 및 이해관계자가 모두 함께 지속가능한 방향으로 나아가기를 목표로 합니다.</p>
+          <h2 className="modal-text-title">이사회 중심 경영</h2>
+          <p className="modal-text"> SK 그룹의 각 회사는 이사회 중심의 경영을 통해 대주주 및 경영진으로부터 독립성을 유지하고 있습니다. 지주회사인 SK주식회사에서는 크게 3개의 위원회가 있으며 거버넌스위원회, 감사위원회, 사외이사후보추천위원회로 구성되어 투명경영을 실천하고 있습니다.</p>
+          <img className="small-img" src="https://github.com/mooyeon-choi/webzine/blob/main/src/images/%EA%B7%B8%EB%A6%BC2.png?raw=true" alt="env-result"/>
         </header>
       </article>
     )
--- src/components/social.jsx
+++ src/components/social.jsx
@@ -19,9 +19,21 @@ export default class Social extends Component {
           <img className="center-img" src="https://post-phinf.pstatic.net/MjAyMDAxMzFfMTE0/MDAxNTgwNDMyODYxMTk2.Im_kJ7v7OBKD2bPHNW1lZXrb7MZ-wc4DHTXiHpC44k4g.42wzhPA0n0dKmkd4Wa41-nOA4M_6mODtzkRJVZCVnHcg.JPEG/80557_50707_557.jpg?type=w1200" alt="deep change"/>
           <h2 className="modal-text-title">SK - '딥 체인지' 인재 육성할 'SK 대학' 출범</h2>
           <p className="modal-text"></p>
+          <h2 className="modal-text-title">사회 안전망 관리</h2>
+          <img className="center-img" src="https://github.com/mooyeon-choi/webzine/blob/main/src/images/blood.png?raw=true" alt="hynix"/>
+          <h2 className="modal-text-title">SK 텔레콤 </h2>
+          <p className="modal-text">혈액 부족 해결 위한 대규모 '헌혈 릴레이'</p>
           <img className="center-img" src="https://github.com/mooyeon-choi/webzine/blob/main/src/images/hynix.png?raw=true" alt="hynix"/>
           <h2 className="modal-text-title">SK 하이닉스</h2>
           <p className="modal-text"> 독거 노인 어르신을 위한 인공지능 스피커 '실버프렌드' 무상 지원, 치매 어르신과 발달장애 아동을 위한 위치주적 기반 배회감지기 '행복 GPS' 보급</p>
+          <img className="center-img" src="https://github.com/mooyeon-choi/webzine/blob/main/src/images/KakaoTalk_20210107_093924105.png?raw=true" alt="hynix"/>
+          <h2 className="modal-text-title">SK</h2>
+          <p className="modal-text">'한끼 나눔 온택트 프로젝트'</p>
+          <img className="center-img" src="https://github.com/mooyeon-choi/webzine/blob/main/src/images/KakaoTalk_20210107_093923800.png?raw=true" alt="hynix"/>
+          <img className="center-img" src="https://github.com/mooyeon-choi/webzine/blob/main/src/images/KakaoTalk_20210107_093923548.png?raw=true" alt="hynix"/>
+          <h2 className="modal-text-title">SK이노베이션</h2>
+          <p className="modal-text">저소득 청소년 학습용 노트북 PC 전달, 독거 노인 결식 방지 SK행복생필품꾸러미 선물, 취약 계층 여름용 쿨 마스크 제작.지원</p>
+          <h2 className="modal-text-title">동반 성장</h2>
         </header>
       </article>
     )

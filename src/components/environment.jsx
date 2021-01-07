4acdc9755bc8d0a57dc9fa48d390d4a48513faa8
mooyeon-choi <dus1208@ajou.ac.kr>
2021-01-07 15:54:02 +0900

    Update 소제목 가운데 정렬, image 추가

--- src/components/environment.jsx
+++ src/components/environment.jsx
@@ -16,14 +16,15 @@ export default class Environment extends Component {
         <h1 className="modal-header-title">SK의 ESG 실천노력</h1>
         <h1 className="modal-header-subtitle">ESG와 관련해 SK 그룹이 해 온/해 나갈 것들</h1>
           <h2 className="modal-header-subtitle">Environment</h2>
-          <img className="center-img" src="https://github.com/mooyeon-choi/webzine/blob/main/src/images/re100.jpg?raw=true" alt="re100"/>
+          <p className="modal-text"></p>
           <h2 className="modal-text-title">SK는 한국 최초로 RE100에 가입하였습니다. </h2>
+          <img className="center-img" src="https://github.com/mooyeon-choi/webzine/blob/main/src/images/re100.jpg?raw=true" alt="re100"/>
           <p className="modal-text">RE100이란, Renewable Energy, 즉 재생에너지를 2050년까지 소비전력의 100퍼센트의 비율로 사용하겠다고 선언한 글로벌 캠페인입니다. RE100은 수출 경쟁력과도 직결되어 sk의 글로벌 경쟁력을 확보 할 수 있는 중요한 요소중 하나입니다.</p>
-          <img className="center-img" src="https://img.asiatoday.co.kr/file/2020y/10m/23d/2020102201001976600130961.jpg" alt="h"/>
           <h2 className="modal-text-title">수소관련- 주식회사/건설 </h2>
+          <img className="center-img" src="https://img.asiatoday.co.kr/file/2020y/10m/23d/2020102201001976600130961.jpg" alt="h"/>
           <p className="modal-text">sk주식회사는 친환경 에너지로 성장하고있는 수소사업에 본격적으로 진출하겠다고 선언했습니다. 에너지 관련회사인 sk innovation, sk e&s 등 관계사 인력으로 구성된 수소사업추진단을 신설하여 사업 추진전략을 실행할 예정입니다.</p>
-          <img className="center-img" src="https://insight-prd-data.s3.ap-northeast-2.amazonaws.com/wp-content/uploads/2/cfile27.uf.0215234D51353F0C0D4709.jpg" alt="cloud bems"/>
           <h2 className="modal-text-title">ICT관련 -skt</h2>
+          <img className="center-img" src="https://insight-prd-data.s3.ap-northeast-2.amazonaws.com/wp-content/uploads/2/cfile27.uf.0215234D51353F0C0D4709.jpg" alt="cloud bems"/>
           <p className="modal-text">sk텔레콤은 지난 2011년부터 에너지 사용을 자동으로 최적화하는 ‘클라우드 BEMS’를 개발, 사용하고 있습니다. 이는 건물 내 조명 냉·난방기 등의 센서 및 네트워크를 기반으로 중앙관리센터에 연결, 빌딩의 전력·에너지 사용량을 실시간으로 분석하는 시스템입니다.</p>
           <h2 className="modal-text-title">환경, 사회 성과 및 목표</h2>
           <img className="center-img" src="https://github.com/mooyeon-choi/webzine/blob/main/src/images/KakaoTalk_20210107_144305742.png?raw=true" alt="env-result"/>
--- src/components/whatIsESG.jsx
+++ src/components/whatIsESG.jsx
@@ -22,7 +22,9 @@ export default class WhatIsESG extends Component {
           <p className="modal-text-middle">ESG란 기존에 재무적 성과만을 추구하던 기업들이 지속가능한 성장을 이루기 위해 고려하기 시작한 비재무적 요소들입니다. 각각 Environment, Social, Governance의 줄임말로, 우리말로 바꾸면 환경, 사회, 지배구조와 관련된 개념이라고 할 수 있습니다.</p>
           <p className="modal-text"> 환경 측면에는 기후변화 영향, 친환경 제품 개발 등의 요소를 꼽을 수 있으며, 사회 측면에는 공정한 채용, 산업안전, 공정한 경쟁 등의 요소가 있고, 마지막으로 지배구조의 측면에는 주주 권리 실현, 이사회 투명성 등을 고려하게 됩니다.</p>
           <img className="center-img" src="https://github.com/mooyeon-choi/webzine/blob/main/src/images/esg02.png?raw=true" alt="esg"/>
-          <img className="center-img" src="https://github.com/mooyeon-choi/webzine/blob/main/src/images/KakaoTalk_20210107_142245990_01.png?raw=true" alt="esg"/>
+          <p className="modal-text"></p>
+          <img className="center-img" src="https://github.com/mooyeon-choi/webzine/blob/main/src/images/KakaoTalk_20210107_142245990_01.png?raw=true" alt="graph"/>
+          <p className="modal-text">투자자들은 더 이상 재무적 지표만으로 기업의 가치를 판단하여 기업에 투자하지 않습니다. 기업이 사회적 가치를 창출하고 있는지, ESG라는 사회적 요구를 수용하고 있는지 등 비재무적인 요소를 적극적으로 판단하여 투자 관점을 적극적으로 변화시키고 있습니다. 글로벌 ESG 펀드의 설정액과 개수가 2010년대 후반으로 접어들면서 가파르게 성장하고 있는 모습을 볼 수 있습니다. 따라서 SK도 이러한 사회적 요구의 변화를 적극적으로 받아들여 ESG를 실천할 필요가 있는 것입니다.</p>
         </header>
       </article>
     )
